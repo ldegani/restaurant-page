472cbb23ab6dc0d07273ca89449b276682fbe475
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-09-03 15:44:59 -0300

    Add headings to the sections

--- src/home.js
+++ src/home.js
@@ -27,15 +27,30 @@ const homePageLoad = () => {
   const imgOne = document.createElement('img');
   imgOne.src = '../src/img/moka-black-coffee.jpg';
   sectionOne.appendChild(imgOne);
+  
+  const divSectionOne = document.createElement('div');
+  divSectionOne.classList.add('container-sections');
+  sectionOne.appendChild(divSectionOne);
 
+  const headingParaOne = document.createElement('span');
+  headingParaOne.textContent = 'Our Mission:';
+  divSectionOne.appendChild(headingParaOne);
 
   const paragraphOne = document.createElement('p');
   paragraphOne.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse voluptates eveniet dignissimos voluptatibus, perspiciatis tempora, id quia quos reprehenderit sapiente explicabo qui rerum facilis. Veritatis reiciendis nisi atque fuga inventore!';
-  sectionOne.appendChild(paragraphOne);
+  divSectionOne.appendChild(paragraphOne);
+
+  const divSectionTwo = document.createElement('div');
+  divSectionTwo.classList.add('container-sections');
+  sectionTwo.appendChild(divSectionTwo);
+
+  const headingParaTwo = document.createElement('span');
+  headingParaTwo.textContent = 'Our Values';
+  divSectionTwo.appendChild(headingParaTwo);
 
   const paragraphTwo = document.createElement('p');
   paragraphTwo.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis reiciendis nisi atque fuga inventore! Esse voluptates eveniet dignissimos voluptatibus, perspiciatis tempora, id quia quos reprehenderit sapiente explicabo qui rerum facilis.'
-  sectionTwo.appendChild(paragraphTwo);
+  divSectionTwo.appendChild(paragraphTwo);
 
   const imgTwo = document.createElement('img');
   imgTwo.src = '../src/img/coffee-filters.jpg';
